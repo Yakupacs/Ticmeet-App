d6072fcc0a13fc116181cd52835706f3881dd73f
yakupacs <ykpacs@gmail.com>
2023-03-27 03:19:53 +0300

    Firebase added.

--- Android-Ticmeet/AndroidTicmeet/app/src/main/java/com/mtaner/android_ticmeet/ActivityGirisYap.java
+++ Android-Ticmeet/AndroidTicmeet/app/src/main/java/com/mtaner/android_ticmeet/ActivityGirisYap.java
@@ -1,14 +1,57 @@
 package com.mtaner.android_ticmeet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityGirisYap extends AppCompatActivity {
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris_yap);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailField = findViewById(R.id.editTextEmail2);
+        mPasswordField = findViewById(R.id.editTextTextPassword);
+        mLoginButton = findViewById(R.id.btnGirisYap);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmailField.getText().toString();
+                String password = mPasswordField.getText().toString();
+                signIn(email, password);
+            }
+        });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            System.out.println("Başarılı");
+                        } else {
+                            System.out.println("Başarısız");
+                        }
+                    }
+                });
     }
 }
